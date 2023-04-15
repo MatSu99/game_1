e8512334aa37a338ea103dd53e96787f59bf6ade
Mat <109213225+MatSu99@users.noreply.github.com>
2023-04-15 22:08:49 +0200

    [DEV] Player vs player mode added
    All functionalities moved to proper functions

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
+# Data
+
+acceptable_input = ["1", "2", "3", "4", "r", "p", "s", "g",\
+                     "rock", "paper", "scissors", "shotgun"]
+names_objects = ["rock", "paper", "scissors", "shotgun"]
+
+WELCOME_MESSAGE = "Rock\nPaper\nScissors\nSHOTGUN"
+player_one_input = None
+player_two_input = None
+__p1_shells__ = 3
+__p2_shells__ = 3
+player_one_lives_counter = 0
+player_two_lives_counter = 0
 
 # Functions
+
+def set_players_live_counters(A, B):
+    global player_one_lives_counter
+    player_one_lives_counter = A
+    global player_two_lives_counter
+    player_two_lives_counter = B
+
+def display_clash(A, B):
+    p1_display = names_objects[A-1]
+    p2_display = names_objects[B-1]
+    print(f">>>{p1_display}<<< VS >>>{p2_display}<<<")
 
 #  @ int A, int B
 # 1 = ROC, 2 = PAPER, 3 = SCISSORS, 4 = SHOTGUN
@@ -45,30 +69,118 @@ def evaluate_players_choices(A, B):
 
 # @ string A, string B
 # converts user input to integers associated with game objects
-# > No output
-# ! Modifies entered variables STRING to INT
+# > int result_1, result_2
 def proccess_users_input(A, B):
+    result_1 = None
+    result_2 = None
     if A == "1" or A == "r" or A == "rock":
-        A = 1
+        result_1 = 1
     elif A == "2" or A == "p" or A == "paper":
-        A = 2
+        result_1 = 2
     elif A == "3" or A == "s" or A == "scissors":
-        A = 3
+        result_1 = 3
     elif A == "4" or A == "g" or A == "shotgun":
-        A = 4
+        result_1 = 4
     else:
-        A = 5
+        result_1 = 5
 
     if B == "1" or B == "r" or B == "rock":
-        B = 1
+        result_2 = 1
     elif B == "2" or B == "p" or B == "paper":
-        B = 2
+        result_2 = 2
     elif B == "3" or B == "s" or B == "scissors":
-        B = 3
+        result_2 = 3
     elif B == "4" or B == "g" or B == "shotgun":
-        B = 4
+        result_2 = 4
     else:
-        B = 5
+        result_2 = 5
+    return result_1, result_2
+
+def check_user_input(A):
+    for x in acceptable_input:
+        if A == x:
+            return True
+    return False
+
+def game_player_vs_player():
+    global player_one_lives_counter
+    global player_two_lives_counter
+    # TODO: Add decsision setting live counters
+    set_players_live_counters(3,3)
+    rounds_counter = 1
+    while player_one_lives_counter > 0 and player_two_lives_counter > 0:
+        print(f"Round number: {rounds_counter}")
+        while True:
+            print("Player one! Enter you choice!")
+            player_one_input = input(">")
+            if check_user_input(player_one_input) == True:
+                break
+            else:
+                print("Incorrect input, valid options are:")
+                print(acceptable_input)
+
+        while True:
+            print("Player two! Enter you choice!")
+            player_two_input = input(">")
+            if check_user_input(player_two_input) == True:
+                break
+            else:
+                print("Incorrect input, valid options are:")
+                print(acceptable_input)
+
+        player_one_choice, player_two_choice = proccess_users_input(player_one_input, player_two_input)
+        display_clash(player_one_choice, player_two_choice)
+        result_phase_1 = check_ammo(player_one_choice, player_two_choice)
+        if result_phase_1 == 1:
+            player_two_lives_counter -= 1
+            print("ROUND ENDED\nWINNER: PLAYER ONE")
+            rounds_counter += 1
+            continue
+        elif result_phase_1 == 2:
+            player_one_lives_counter -= 1
+            print("ROUND ENDED\nWINNER: PLAYER TWO")
+            rounds_counter += 1
+            continue
+        elif result_phase_1 == 3:
+            player_one_lives_counter -= 1
+            player_two_lives_counter -= 1
+            print("DRAW\nBOTH PLAYER LOSE 1 health point!")
+            rounds_counter += 1
+            continue
+        elif result_phase_1 == 3:
+            print("DRAW\n")
+            rounds_counter += 1
+            continue
+        else:
+            pass
+
+        result_phase_2 = proccess_users_input(player_one_choice, player_two_choice)
+        if result_phase_2 == 1:
+            player_two_lives_counter -= 1
+            print("ROUND ENDED\nWINNER: PLAYER ONE")
+            rounds_counter += 1
+            continue
+        elif result_phase_2 == 2:
+            player_one_lives_counter -= 1
+            print("ROUND ENDED\nWINNER: PLAYER TWO")
+            rounds_counter += 1
+            continue
+        else:
+            print("DRAW\n")
+            rounds_counter += 1
+            continue
+
+    
+    if player_one_lives_counter == 0 and player_two_lives_counter > 0:
+        print("END OF THE GAME, THE WINNER IS:\nPLAYER TWO")
+    elif player_two_lives_counter == 0 and player_one_lives_counter > 0:
+        print("END OF THE GAME, THE WINNER IS:\nPLAYER TWO")
+    elif player_one_lives_counter == 0 and player_two_lives_counter == 0:
+        print("END OF THE GAME, DRAW")
+    else:
+        print("ERROR")
+
+
 
 # @ int A, int B
 # converts user input to integers associated with game objects
@@ -78,6 +190,10 @@ def proccess_users_input(A, B):
 def check_ammo(A, B):
     player_one_status = 1
     player_two_status = 1
+    global __p1_shells__
+    global __p2_shells__
+    global player_one_lives_counter
+    global player_two_lives_counter
     if A != 4 and B != 4:
         return 0
     else:
@@ -88,8 +204,8 @@ def check_ammo(A, B):
                 player_one_status = 0
 
         if B == 4:
-            if __p1_shells__ > 0:
-                __p1_shells__-= 1
+            if __p2_shells__ > 0:
+                __p2_shells__-= 1
             else:
                 player_two_status = 0
 
@@ -101,26 +217,9 @@ def check_ammo(A, B):
             return 1
         else:
             return 2
-    return 5
+    return 5 # Consider delete, not achivable now
         
-
-
+# Main flow start
 print("START")
-
-# Input variables
-WELCOME_MESSAGE = "Rock\nPaper\nScissors\nSHOTGUN"
-player_one_input = None
-player_two_input = None
-__p1_shells__ = 3
-__p2_shells__ = 3
-
-print("Player one! Enter you choice!")
-player_one_input = input(">")
-
-print("Player two! Enter you choice!")
-player_two_input = input(">")
-
-proccess_users_input(player_one_input, player_two_input)
-check_ammo(player_one_input, player_two_input)
-
-print(f"{player_one_input} VS {player_two_input}")
+print(WELCOME_MESSAGE)
+game_player_vs_player()
